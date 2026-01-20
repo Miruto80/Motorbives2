c7ab7c87bd9e350f032f2173c15118493c29b143
Miruto80 <miguelfernando8012@gmail.com>
2026-01-20 11:43:17 -0400

    Carros

--- src/assets/data/cars.js
+++ src/assets/data/cars.js
@@ -1,74 +1,206 @@
 const cars = [
 {
     id: 1,
-    name: 'Camry',
-    make: 'Toyota',
-    year: 2020,
-    type: 'Sedan',
-    downpayment: 3000,
-    financingprice: 22000,
-    cashprice: 25000,
-    mileage: 15000,
-    description: 'A reliable and fuel-efficient sedan with advanced safety features.',
+    name: 'M2 106',
+    make: 'Freightliner',
+    year: 2017,
+    type: 'Box-trucks',
+    downpayment: 6000,
+    financingprice: 34000,
+    cashprice: 32000,
+    mileage: 183000,
+      description: `- Clean Title
+      - Perfect for Heavy-Duty Work
+      - 185,000 Miles
+      - Cummins Engine
+      - No CDL Required
+      - GVWR: 25,999 lbs
+      - Allison Automatic Transmission
+      - 26 ft Box
+      - Aluminum Liftgate
+      - Air Suspension
+      - Air Brakes
+      - Tires at 90% Life
+      - Immaculate Condition
+      - Financing Available
+      `,
     images: [
-      '/img/logo con fondo.webp',
-      '/img/Logo en negro.jpeg',
-      '/img/Logo en negro.jpeg',
-      '/img/Logo en negro.jpeg',
-      '/img/Logo en negro.jpeg',
-      '/img/Logo en negro.jpeg',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (1).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (2).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (3).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (4).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (5).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (6).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (7).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (8).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (9).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (10).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (11).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (12).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (13).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (14).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (15).webp',
+      '/Box-trucks/FREIGHTLINER M2 106 2017 (16).webp',
     ],
 },
 {
     id: 2,
-    name: 'LE',
-    make: 'Toyota',
-    year: 2020,
-    type: 'Sedan',
-    downpayment: 3000,
-    financingprice: 22000,
-    cashprice: 25000,
-    mileage: 15000,
-    description: 'A reliable and fuel-efficient sedan with advanced safety features.',
+    name: 'EXPLORER SPORT 4x4',
+    make: 'Ford',
+    year: 2015,
+    type: 'Suvs',
+    downpayment: 2500,
+    financingprice: 10900,
+    cashprice: 9900,
+    mileage: 137000,
+    description: `- Clean Title
+- Perfect for the Family
+- 137,000 Miles
+- 3 Rows of Seats
+- Leather Seats
+- Touchscreen Display
+- Backup Camera
+- Dual Sunroof
+- Cold & Hot A/C
+- Immaculate Condition
+- Luxury Rims
+- Financing Available
+      `,
     images: [
-      '/img/logo con fondo.webp',
-      '/img/Logo en negro.jpeg',
-      '/img/Logo en negro.jpeg',
-      '/img/Logo en negro.jpeg',
-      '/img/Logo en negro.jpeg',
-      '/img/Logo en negro.jpeg',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (1).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (2).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (3).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (4).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (5).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (6).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (7).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (8).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (9).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (10).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (11).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (12).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (13).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (14).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (15).webp',
+      '/Suvs/FORD EXPLORER SPORT 4x4 2015 (16).webp',
     ],
 },
 {
     id: 3,
-    name: 'le',
-    make:'Chevrolet' ,
-    year: 2021,
-    type: 'Suv',
+    name: 'F150 LARIAT 4x4',
+    make:'Ford',
+    year: 2018,
+    type: 'Trucks',
     downpayment: 3000,
-    financingprice: 22000,
+    financingprice: 28900,
     cashprice: 25000,
-    mileage: 15000,
-    description: 'A reliable and fuel-efficient sedan with advanced safety features.',
+    mileage: 101000,
+   description: `- Clean Title
+- Luxury Rims
+- Brand New Tires
+- Immaculate Condition
+- One Owner
+- Cold & Hot A/C
+- Backup Camera
+- Lifted Suspension
+- 8-Cylinder 6.0L Engine
+- Ready for Heavy-Duty Work
+- Financing Available
+      `,
     images: [
-      '/img/Logo en negro.jpeg',
-      '/img/logo con fondo.webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (1).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (2).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (3).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (4).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (5).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (6).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (7).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (8).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (9).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (10).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (11).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (12).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (13).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (14).webp',
+      '/Trucks/FORD F150 LARIAT 4x4 2018 (15).webp',
     ],
 },
 {
     id: 4,
-    name: 'le',
-    make: 'Hino',
+    name: 'G2500 EXPRESS',
+    make: 'Chevrolet',
     year: 2021,
-     type: 'Box-truck',
-    downpayment: 3000,
-    financingprice: 22000,
-    cashprice: 25000,
-    mileage: 15000,
-    description: 'A reliable and fuel-efficient sedan with advanced safety features.',
+     type: 'Vans',
+    downpayment: 2500,
+    financingprice: 22900,
+    cashprice: 21000,
+    mileage: 64000,
+  description: `- Clean Title
+- Perfect for Heavy-Duty Work
+- Backup Camera
+- 66,000 Miles
+- Luxury Rims
+- 6-Cylinder 4.3L Engine
+- Cold & Hot A/C
+- Tires at 100% Life
+- One Owner
+- Immaculate Condition
+      `,
     images: [
-      '/img/Logo en negro.jpeg',
-      '/img/logo con fondo.webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (1).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (2).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (3).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (4).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (5).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (6).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (7).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (8).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (9).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (10).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (11).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (12).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (13).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (14).webp',
+      '/Vans/CHEVROLET G2500 EXPRESS 2021 (15).webp',
+    ],
+},
+{
+    id: 5,
+    name: 'TRANSIT 250',
+    make: 'Ford',
+    year: 2021,
+     type: 'Vans',
+    downpayment: 3000,
+    financingprice: 27000,
+    cashprice: 25000,
+    mileage: 84000,
+  description: `- Clean Title
+- Excellent Condition
+- Touchscreen Display
+- Tires at 90% Life
+- One Owner
+- Cold & Hot A/C
+- Backup Camera
+- Gasoline Engine
+- Immaculate Condition
+- Financing Available
+      `,
+    images: [
+      '/Vans/FORD TRANSIT 250 2021 (1).webp',
+      '/Vans/FORD TRANSIT 250 2021 (2).webp',
+      '/Vans/FORD TRANSIT 250 2021 (3).webp',
+      '/Vans/FORD TRANSIT 250 2021 (4).webp',
+      '/Vans/FORD TRANSIT 250 2021 (5).webp',
+      '/Vans/FORD TRANSIT 250 2021 (6).webp',
+      '/Vans/FORD TRANSIT 250 2021 (7).webp',
+      '/Vans/FORD TRANSIT 250 2021 (8).webp',
+      '/Vans/FORD TRANSIT 250 2021 (9).webp',
+      '/Vans/FORD TRANSIT 250 2021 (10).webp',
+      '/Vans/FORD TRANSIT 250 2021 (11).webp',
+      '/Vans/FORD TRANSIT 250 2021 (12).webp',
+      '/Vans/FORD TRANSIT 250 2021 (13).webp',
+      '/Vans/FORD TRANSIT 250 2021 (14).webp',
+      '/Vans/FORD TRANSIT 250 2021 (15).webp',
     ],
 },
 
